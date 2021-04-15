d6a0e9f48d5cf4c5dc1a7936066c07daf01ccaa4
MAZHAR <iiestshibpur605@gmail.com>
2021-04-15 12:18:36 +0530

    Destructor in C++

---
+++ Destructor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+    Deconstructor in C++
+    Deconstructor gives us a completely well defined structured way of managing the life time of every object in a clean manner.
+    -It is implicitly called when a class object goes out of scope or by operator delete (later we will know this).
+    -No parameter is allowed.
+    -Can't be overloaded
+    -Unique for the class (Since no parameter allowed -> Hence no overloading allowed -> Hence, Unique)
+    -If user provided no destructor, default destructor is called by compiler (which has nothing in its body)
+*/
+
+class Stack {
+private:
+    char *data_;
+    int top_;
+public:
+    Stack(): data_(new char[10]), top_(-1) {}
+
+    ~Stack();
+
+};
+
+Stack::~Stack() {
+    cout << "Destructor called\n";
+    delete data_;
+}
+
+//Application
+int main() {
+    Stack s;
+
+
+return 0;
+}
