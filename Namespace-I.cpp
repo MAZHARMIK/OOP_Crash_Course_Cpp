301e0de807d869195104ea60dba02e95ef27741c
MAZHAR <iiestshibpur605@gmail.com>
2021-04-22 23:40:14 +0530

    Namespace in C++ - I

---
+++ Namespace-I.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Namespace in C++ - I
+    -It's a scope for the identifiers(functions, variables etc)
+    -It's use to organise the code into logical groups
+    -Prevent name collisions that can occur especially when your code base includes multiple libraries.
+*/
+
+namespace MyNameSpace {
+    int myData;
+    void myFunction() {
+        cout << "MyNamSpace myFunction" << endl;
+    }
+    class MyClass {
+        int data;
+    public:
+        MyClass(int d) : data(d) {}
+        void display() {
+            cout << "MyClass data = " << data << endl;
+        }
+    };
+}
+
+int main() {
+    MyNameSpace::myData = 10;
+    cout << "MyNameSpace::myData = " << MyNameSpace::myData << endl;
+
+    myFunction(); //Error!!!!!!!!!!
+    MyNameSpace::myFunction();
+
+    MyNameSpace::MyClass obj(25);
+    obj.display();
+
+return 0;
+}
+
