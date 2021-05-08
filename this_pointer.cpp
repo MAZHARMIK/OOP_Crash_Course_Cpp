9eecfab483b9689797dcad9418020180578a81e9
MAZHAR <iiestshibpur605@gmail.com>
2021-05-08 15:34:03 +0530

    Update this_pointer.cpp

--- this_pointer.cpp
+++ this_pointer.cpp
@@ -10,7 +10,8 @@ using namespace std;
 //It specifies every object by its own address
 
 //What is the type of "this" pointer ?
-//Ans : Type of "this" pointer of class X is : X const* this;
+//Ans : For a class X, the type of this pointer is ‘X* ‘.
+//      Also, if a member function of X is declared as const, then the type of this pointer is ‘const X *’
 class X{
     public:
         int x = 5;
